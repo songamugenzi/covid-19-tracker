f3f5ca4961cff494727a359b5a02bf362235af99
S Mugenzi <smugenzi@Ss-MacBook-Air.local>
2021-07-06 16:59:02 -0400

    created the Cards, Chart, and CountryPicker components

---
+++ src/components/Cards/Cards.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Cards = () => {
+    return (
+        <div>
+            <h1>This is Cards.js</h1>
+        </div>
+    )
+}
+
+export default Cards
---
+++ src/components/CountryPicker/CountryPicker.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CountryPicker = () => {
+    return (
+        <div>
+            <h1>This is CountryPicker.js</h1>
+        </div>
+    )
+}
+
+export default CountryPicker
