f9b5f2768abce2ac57d72a58f9b68deefe27921d
S Mugenzi <smugenzi@Ss-MacBook-Air.local>
2021-07-07 01:13:38 -0400

    successfully deployed to netlify

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # covid-19-tracker
 Coronavirus tracker with the latest statistics of confirmed cases, recoveries, and deaths across the U.S and globally
+
+[![Netlify Status](https://api.netlify.com/api/v1/badges/a5c2ac8a-670b-4883-bdf6-c1b20e4571e5/deploy-status)](https://app.netlify.com/sites/covid19-trackr/deploys)
--- src/components/CountryPicker/CountryPicker.js
+++ src/components/CountryPicker/CountryPicker.js
@@ -15,7 +15,7 @@ const CountryPicker = ({ handleCountryChange }) => {
     }, []);
 
     return (
-        <FormControl className={styles.formControl}>
+        <FormControl className={styles.formContainer}>
             <NativeSelect defaultValue='' onChange={(e) => handleCountryChange(e.target.value)}>
                 <option value=''>Global</option>
                 {countries.map((country, i) => <option key={i} value={country}>{country}</option>)}
